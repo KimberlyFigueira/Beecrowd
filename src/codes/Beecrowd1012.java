b08b1d98394fa8984987dcd52add01104c8d6292
Kimberly Figueira <kimhoran56@gmail.com>
2024-10-11 14:23:06 -0300

    Beecrowd 1012

--- src/codes/Beecrowd1012.java
+++ src/codes/Beecrowd1012.java
@@ -1,7 +1,13 @@
 package codes;
 
+import java.util.Scanner;
+
 public class Beecrowd1012 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+        double C = input.nextDouble();
     }
 }
